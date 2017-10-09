a3cf292e65f82d59c64f1f9c1a22660f72c8d747
leo2n <ttxs.an@gmail.com>
2017-10-09 20:50:23 +0800

    ping server using Python

---
+++ ping_server.py
@@ -0,0 +1,15 @@
+"""
+ping server using Python
+"""
+import platform
+import os 
+
+def ping(host):
+    """
+    Return True if host: str  responds to a ping request 
+    """
+    parameters = "-n 1" if platform.system().lower() == "windows" else "-c 1"
+
+    return os.system("ping " + parameters +" " +host) == 0
+
+ping("127.0.0.1")
